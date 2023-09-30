062fece91bb85d161f00fb3affc952085eb64a81
francesco <francesco@192.168.1.3>
2023-09-30 13:47:01 +0200

    mofiche di prova per la console ocp

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
- print " mofidiche effettiate!  \n" ;
+print " mofidiche effettiate!  \n" ;
+print " the change is in the air \n" ;
 ?>
